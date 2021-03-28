b58212ea96e2546fa6c3960116207cf60efa65c5
katderv <kat.derv98@gmail.com>
2021-03-28 16:45:44 +0300

    Update login.java

--- src/login.java
+++ src/login.java
@@ -36,6 +36,7 @@ public class login {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 	}
 
 }
