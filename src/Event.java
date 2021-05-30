0ceb4fd7ec09e1cc92c7b6538d1472b264837ea4
ariadni-machia <81858190+ariadni-machia@users.noreply.github.com>
2021-05-30 18:35:02 +0300

    Event & User Class

---
+++ src/Event.java
@@ -0,0 +1,46 @@
+import java.time.LocalDate; // import the LocalDate class
+import java.time.LocalTime; // import the LocalTime class
+public class Event {
+	private LocalDate day;
+	private LocalTime time;
+	private String title;
+	
+	public Event(String day, String time, String title) { // Constructor with all String arguments
+		this.day=LocalDate.parse(day);
+		this.time=LocalTime.parse(time);
+		this.title=title;
+	}
+	public Event() { // Constructor with no arguments get current date & time and title is null
+		this.day=LocalDate.now();
+		this.time=LocalTime.now();
+		this.title=null;
+	}
+	public void printEvent() { //prints details of Event
+		System.out.println("day: "+ this.day+" time:"+ this.time+ " title: "+ this.title);
+	}
+	
+	public void addReminder() { // Adds a notification for the Event
+		
+	}
+	/*Getters */
+	public LocalDate getDay() {
+		return this.day;
+	}
+	public LocalTime getTime() {
+		return this.time;
+	}
+	public String getTitle() {
+		return this.title;
+	}
+	/*Setters */
+	public void setDay(String day) {
+		this.day=LocalDate.parse(day);
+	}	
+	public void setTime(String time) {
+		this.time=LocalTime.parse(time);
+	}	
+	public void setTitle(String title) {
+		this.title=title;
+	}
+}
+
---
+++ src/User.java
@@ -0,0 +1,39 @@
+import java.time.LocalDate; // import the LocalDate class
+import java.time.LocalTime;
+import java.io.File;  // Import the File class
+
+enum Gender {
+	  Female,
+	  Male,
+	  Other
+	}
+
+public class User {
+	private String email;
+	private String password;
+	private String name;
+	private String surname;
+	private LocalDate dateOfBirth;
+	private Gender gender;
+	private String city;
+	private File profPhoto;
+	
+	public String getUserType(Object obj) { // Return user's type as a String (Patient or Doctor)
+		return obj.getClass().getName();
+	}
+	
+	public User(String email, String password, String name, String surname, String dateOfBirth, Gender gender, String city, File profPhot) {
+		this.email=email;
+		this.password=password;
+		this.name=name;
+		this.surname=surname;
+		this.dateOfBirth=LocalDate.parse(dateOfBirth);
+		this.gender=gender;
+		this.city=city;
+		this.profPhoto=profPhoto;
+	}
+	public void printUser() {
+		System.out.println("email: "+this.email+"\n password: "+this.password+"\n name: "+this.name+"\n surname: "+this.surname+"\n date of Birth: "+this.dateOfBirth+"\n gender: "+this.gender+"\n city: "+this.city);
+		System.out.println("\n File: "+this.profPhoto);
+	}
+}
