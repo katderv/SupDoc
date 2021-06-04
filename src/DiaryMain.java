d9fe05523fa8f9dfe094d07e850fec9b170e3949
katderv <kat.derv98@gmail.com>
2021-06-04 16:47:47 +0300

    Update DiaryMain.java

--- src/DiaryMain.java
+++ src/DiaryMain.java
@@ -35,14 +35,23 @@ public class DiaryMain {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("\u0397\u03BC\u03B5\u03C1\u03BF\u03BB\u03CC\u03B3\u03B9\u03BF \u03A3\u03C5\u03BC\u03C0\u03C4\u03C9\u03BC\u03B1\u03C4\u03BF\u03BB\u03BF\u03B3\u03AF\u03B1\u03C2");
+		JButton btnNewButton_1 = new JButton("+");
+		btnNewButton_1.setBackground(SystemColor.inactiveCaption);
+		btnNewButton_1.setForeground(Color.WHITE);
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		btnNewButton_1.setBounds(213, 0, 51, 38);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JLabel lblNewLabel = new JLabel("\u0397\u03BC\u03B5\u03C1\u03BF\u03BB\u03CC\u03B3\u03B9\u03BF \u03A3\u03C5\u03BC\u03C0\u03C4\u03C9\u03BC\u03B1\u03C4\u03BF\u03BB\u03BF\u03B3\u03AF\u03B1\u03C2           ");
 		lblNewLabel.setForeground(new Color(245, 245, 245));
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 15));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel.setOpaque(true);
 		lblNewLabel.setBackground(SystemColor.inactiveCaption);
 		lblNewLabel.setBounds(0, 0, 264, 38);
 		frame.getContentPane().add(lblNewLabel);
+		
+	
 		
 		
 		ArrayList<DiaryEntry> de = patient_menu.diar.entry;
@@ -51,7 +60,7 @@ public class DiaryMain {
 			btnNewButton.setBackground(SystemColor.control);
 			btnNewButton.setHorizontalAlignment(SwingConstants.LEFT);
 			btnNewButton.setForeground(SystemColor.textInactiveText);
-			btnNewButton.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textInactiveText));
+			//btnNewButton.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textInactiveText));
 			btnNewButton.setBounds(0, 37+i*37, 264, 38);
 			frame.getContentPane().add(btnNewButton);
 			}
