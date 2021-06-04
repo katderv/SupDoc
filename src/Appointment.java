21d5af6ed1038406c91489d6be6134d141984d45
AspasiaKoukouvela <76659603+AspasiaKoukouvela@users.noreply.github.com>
2021-06-04 17:30:28 +0300

    add img

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,18 +1,26 @@
-import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.SystemColor;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.MatteBorder;
 
 public class Appointment extends JFrame {
-
-
 
 	private LocalDate day = LocalDate.now();
 	private LocalTime hour = LocalTime.now();
@@ -23,6 +31,8 @@ public class Appointment extends JFrame {
 	
 	
 	public static Connection myConn;
+	JFrame frame = new JFrame();
+	
 	
 	public Appointment () {
 		initialize();
@@ -65,7 +75,7 @@ public class Appointment extends JFrame {
 	}
 	
 	
-public static void getReason () {
+	public static void getReason () {
 		
 		try {
 			myConn = DriverManager.getConnection("jdbc:sqlite:SupDocDB.db");
@@ -83,27 +93,105 @@ public static void getReason () {
 		}
 	}
 	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+
+	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Appointment frame = new Appointment();
-					Appointment ap = new Appointment();
-					ap.getReason();
-					//frame.setVisible(true);
+					Appointment window = new Appointment();
+					window.frame.setVisible(true);
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
-	}
+	}*/
 
 	private void initialize() {
 
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.getContentPane().setBackground(SystemColor.control);
+		frame.setBounds(100, 100, 280, 484);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setBorder(new EmptyBorder(0, 0, 0, 0));
+		btnNewButton.setBackground(SystemColor.inactiveCaption);
+		btnNewButton.setBounds(230, 5, 29, 29);
+		frame.getContentPane().add(btnNewButton);
+		Image img2 = new ImageIcon(this.getClass().getResource("/avatar.png")).getImage();
+		btnNewButton.setIcon(new ImageIcon(img2));
+		
+		JLabel lblNewLabel = new JLabel("");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(20, 55, 220, 195);
+		frame.getContentPane().add(lblNewLabel);
+		Image img = new ImageIcon(this.getClass().getResource("/book_appointment.png")).getImage();
+		Image newImage =  img.getScaledInstance(245, 210, Image.SCALE_DEFAULT);
+		lblNewLabel.setIcon(new ImageIcon(newImage));
+		
+		
+		JLabel lblNewLabel_1 = new JLabel("<html>Ημ/νια</html>");
+		lblNewLabel_1.setForeground(Color.DARK_GRAY);
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_1.setBounds(30, 260, 91, 20);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		
+		JTextField textField = new JTextField();
+		textField.setForeground(SystemColor.textInactiveText);
+		textField.setText("15/03/2020");
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField.setBackground(SystemColor.control);
+		textField.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textInactiveText));
+		textField.setBounds(131, 261, 91, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.addFocusListener(new FocusListener() {
+
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (textField.getText().equals("Όνομα")) {
+                	textField.setText("");
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(textField.getText().isEmpty()) {
+                	textField.setText("Όνομα");
+                }
+            }
+        });
+		///////////////////////
+		
+		JLabel lblNewLabel_2 = new JLabel("<html>Ώρα</html>");
+		lblNewLabel_2.setForeground(Color.DARK_GRAY);
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_2.setBounds(30, 330, 91, 20);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		///////////////////////
+		///combo box probably
+		///////////////////////
+		
+		JLabel lblNewLabel_3 = new JLabel("<html>Λόγος<br/> Επίσκεψης</html>");
+		lblNewLabel_3.setForeground(Color.DARK_GRAY);
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblNewLabel_3.setBounds(35, 400, 91, 35);
+		frame.getContentPane().add(lblNewLabel_3);
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		///////////////////////
+		///combo box probably
+		///////////////////////
 		
 	}
 
