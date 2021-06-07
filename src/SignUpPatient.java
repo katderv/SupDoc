6637a99bcd6c587b6cde2015272cff8812fb1006
ariadni-machia <81858190+ariadni-machia@users.noreply.github.com>
2021-06-07 23:17:22 +0300

    SignUpPatient Working + JCalendar

--- src/SignUpPatient.java
+++ src/SignUpPatient.java
@@ -4,17 +4,33 @@ import java.awt.Image;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.SwingConstants;
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.SystemColor;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import javax.swing.JTextField;
 import javax.swing.border.MatteBorder;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import java.awt.Color;
 import javax.swing.JPasswordField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import com.toedter.calendar.JDateChooser;
 
 public class SignUpPatient {
 
@@ -24,9 +40,9 @@ public class SignUpPatient {
 	private JTextField txtEmail;
 	private JPasswordField passwordField;
 	private JTextField textField_2;
-	private JTextField textField_3;
 	private JTextField textField_4;
 	private JButton btnNewButton_1;
+	private JTextField textField_5;
 
 	/**
 	 * Create the application.
@@ -47,7 +63,7 @@ public class SignUpPatient {
 		
 		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(20, 11, 74, 71);
+		lblNewLabel.setBounds(20, 10, 74, 71);
 		frame.getContentPane().add(lblNewLabel);
 		Image img = new ImageIcon(this.getClass().getResource("/logo_signup.png")).getImage();
 		lblNewLabel.setIcon(new ImageIcon(img));
@@ -55,12 +71,12 @@ public class SignUpPatient {
 		JLabel lblNewLabel_1 = new JLabel("\u0395\u03B3\u03B3\u03C1\u03B1\u03C6\u03AE");
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
-		lblNewLabel_1.setBounds(91, 25, 146, 43);
+		lblNewLabel_1.setBounds(91, 24, 146, 43);
 		frame.getContentPane().add(lblNewLabel_1);
 		
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_2.setBounds(157, 90, 65, 43);
+		lblNewLabel_2.setBounds(157, 89, 65, 43);
 		frame.getContentPane().add(lblNewLabel_2);
 		Image img2 = new ImageIcon(this.getClass().getResource("/photo.png")).getImage();
 		lblNewLabel_2.setIcon(new ImageIcon(img2));
@@ -70,14 +86,14 @@ public class SignUpPatient {
 		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 9));
 		btnNewButton.setForeground(SystemColor.text);
 		btnNewButton.setBackground(SystemColor.textInactiveText);
-		btnNewButton.setBounds(157, 138, 65, 23);
+		btnNewButton.setBounds(157, 137, 65, 23);
 		frame.getContentPane().add(btnNewButton);
 		
 		JLabel lblNewLabel_3 = new JLabel("<html>\u03A0\u03C1\u03BF\u03C3\u03B8\u03AD\u03C3\u03C4\u03B5<br/>\u03A6\u03C9\u03C4\u03BF\u03B3\u03C1\u03B1\u03C6\u03AF\u03B1</html>");
 		lblNewLabel_3.setForeground(Color.DARK_GRAY);
 		lblNewLabel_3.setHorizontalAlignment(SwingConstants.CENTER);
 		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_3.setBounds(20, 104, 127, 40);
+		lblNewLabel_3.setBounds(20, 103, 127, 40);
 		frame.getContentPane().add(lblNewLabel_3);
 		
 		textField = new JTextField();
@@ -86,14 +102,14 @@ public class SignUpPatient {
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		textField.setBackground(SystemColor.control);
 		textField.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textInactiveText));
-		textField.setBounds(38, 183, 184, 20);
+		textField.setBounds(38, 169, 184, 20);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		textField.addFocusListener(new FocusListener() {
 
             @Override
             public void focusGained(FocusEvent e) {
-                if (textField.getText().equals("Όνομα")) {
+                if (textField.getText().equals("\u038C\u03BD\u03BF\u03BC\u03B1")) {
                 	textField.setText("");
                 }
             }
@@ -101,14 +117,14 @@ public class SignUpPatient {
             @Override
             public void focusLost(FocusEvent e) {
                 if(textField.getText().isEmpty()) {
-                	textField.setText("Όνομα");
+                	textField.setText("\u038C\u03BD\u03BF\u03BC\u03B1");
                 }
             }
         });
 		
 		textField_1 = new JTextField();
 		textField_1.setText("\u0395\u03C0\u03CE\u03BD\u03C5\u03BC\u03BF");
-		textField_1.setBounds(38, 214, 182, 20);
+		textField_1.setBounds(38, 200, 184, 20);
 		textField_1.setForeground(SystemColor.textInactiveText);
 		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		textField_1.setBackground(SystemColor.control);
@@ -119,7 +135,7 @@ public class SignUpPatient {
 
             @Override
             public void focusGained(FocusEvent e) {
-                if (textField_1.getText().equals("Επώνυμο")) {
+                if (textField_1.getText().equals("\u0395\u03C0\u03CE\u03BD\u03C5\u03BC\u03BF")) {
                 	textField_1.setText("");
                 }
             }
@@ -127,14 +143,14 @@ public class SignUpPatient {
             @Override
             public void focusLost(FocusEvent e) {
                 if(textField_1.getText().isEmpty()) {
-                	textField_1.setText("Επώνυμο");
+                	textField_1.setText("\u0395\u03C0\u03CE\u03BD\u03C5\u03BC\u03BF");
                 }
             }
         });
 		
 		txtEmail = new JTextField();
 		txtEmail.setText("Email");
-		txtEmail.setBounds(38, 245, 182, 20);
+		txtEmail.setBounds(38, 231, 184, 20);
 		txtEmail.setForeground(SystemColor.textInactiveText);
 		txtEmail.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		txtEmail.setBackground(SystemColor.control);
@@ -164,7 +180,7 @@ public class SignUpPatient {
 		passwordField.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		passwordField.setBackground(SystemColor.control);
 		passwordField.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textInactiveText));
-		passwordField.setBounds(38, 276, 182, 20);
+		passwordField.setBounds(38, 262, 184, 20);
 		passwordField.addFocusListener(new FocusListener() {
 
             @Override
@@ -183,9 +199,10 @@ public class SignUpPatient {
         });
 		frame.getContentPane().add(passwordField);
 		
+		/*// Date of Birth - Old Version
 		textField_2 = new JTextField();
 		textField_2.setText("--/--/--");
-		textField_2.setBounds(38, 307, 182, 20);
+		textField_2.setBounds(0, 293, 182, 20);
 		frame.getContentPane().add(textField_2);
 		textField_2.setColumns(10);
 		textField_2.setForeground(SystemColor.textInactiveText);
@@ -208,36 +225,11 @@ public class SignUpPatient {
             }
         });
 		textField_2.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textInactiveText));
-		
-		textField_3 = new JTextField();
-		textField_3.setText("\u03A6\u03CD\u03BB\u03BF");
-		textField_3.setBounds(38, 338, 182, 20);
-		frame.getContentPane().add(textField_3);
-		textField_3.setColumns(10);
-		textField_3.setForeground(SystemColor.textInactiveText);
-		textField_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		textField_3.setBackground(SystemColor.control);
-		textField_3.addFocusListener(new FocusListener() {
-
-            @Override
-            public void focusGained(FocusEvent e) {
-                if (textField_3.getText().equals("Φύλο")) {
-                	textField_3.setText("");
-                }
-            }
-
-            @Override
-            public void focusLost(FocusEvent e) {
-                if(textField_3.getText().isEmpty()) {
-                	textField_3.setText("Φύλο");
-                }
-            }
-        });
-		textField_3.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textInactiveText));
+		*/
 		
 		textField_4 = new JTextField();
 		textField_4.setText("\u03A0\u03CC\u03BB\u03B7");
-		textField_4.setBounds(38, 369, 182, 20);
+		textField_4.setBounds(38, 355, 184, 20);
 		frame.getContentPane().add(textField_4);
 		textField_4.setColumns(10);
 		textField_4.setForeground(SystemColor.textInactiveText);
@@ -247,7 +239,7 @@ public class SignUpPatient {
 
             @Override
             public void focusGained(FocusEvent e) {
-                if (textField_4.getText().equals("Πόλη")) {
+                if (textField_4.getText().equals("\u03A0\u03CC\u03BB\u03B7")) {
                 	textField_4.setText("");
                 }
             }
@@ -255,17 +247,161 @@ public class SignUpPatient {
             @Override
             public void focusLost(FocusEvent e) {
                 if(textField_4.getText().isEmpty()) {
-                	textField_4.setText("Πόλη");
+                	textField_4.setText("\u03A0\u03CC\u03BB\u03B7");
                 }
             }
         });
 		textField_4.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textInactiveText));
 		
+		textField_5 = new JTextField();
+		textField_5.setText("\u03A0\u03B5\u03C1\u03B9\u03BF\u03C7\u03AE");
+		textField_5.setForeground(SystemColor.textInactiveText);
+		textField_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		textField_5.setColumns(10);
+		textField_5.setBorder(new MatteBorder(0, 0, 1, 0, (Color) SystemColor.textInactiveText));
+		textField_5.setBackground(SystemColor.menu);
+		textField_5.setBounds(38, 385, 184, 20);
+		frame.getContentPane().add(textField_5);
+		textField_5.addFocusListener(new FocusListener() {
+
+            @Override
+            public void focusGained(FocusEvent e) {
+                if (textField_5.getText().equals("\u03A0\u03B5\u03C1\u03B9\u03BF\u03C7\u03AE")) {
+                	textField_5.setText("");
+                }
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if(textField_5.getText().isEmpty()) {
+                	textField_5.setText("\u03A0\u03B5\u03C1\u03B9\u03BF\u03C7\u03AE");
+                }
+            }
+        });
+		
+		
+		JComboBox genderBox = new JComboBox();
+		genderBox.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		genderBox.setModel(new DefaultComboBoxModel(new String[] {"Female", "Male", "Other"}));
+		genderBox.setBounds(38, 324, 184, 21);
+		frame.getContentPane().add(genderBox);
+		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(38, 294, 184, 19);
+		frame.getContentPane().add(dateChooser);
+		
+
 		btnNewButton_1 = new JButton("\u0395\u03B3\u03B3\u03C1\u03B1\u03C6\u03AE");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if(textField.getText().isEmpty()|| textField_1.getText().isEmpty() || passwordField.getText().isEmpty()|| textField_4.getText().isEmpty() ||textField_5.getText().isEmpty()|| ((JTextField)dateChooser.getDateEditor().getUiComponent()).getText().isEmpty() ) {					
+					JOptionPane.showMessageDialog(frame,"Fill in all the fields");
+					return;
+				}				
+				SimpleDateFormat sdf= new SimpleDateFormat("yyyy-MM-dd");				
+				String sel_dob=sdf.format(dateChooser.getDate()); //get date of birth
+				boolean check_dob=false;
+				if(isValidDOB(LocalDate.parse(sel_dob))) {
+					check_dob=true;
+				}
+				else {
+					JOptionPane.showMessageDialog(frame,"Invalid date of birth");
+				}
+				
+				
+				String name = textField.getText();
+				String surname= textField_1.getText();	
+				String password =passwordField.getText();
+				String gender = (String)genderBox.getSelectedItem();
+				String city = textField_4.getText();
+				String region = textField_5.getText();
+				
+				
+				String email=null;
+				if(isValid(txtEmail.getText())) {
+					email= txtEmail.getText(); // make sure valid email
+				}
+				else {
+					JOptionPane.showMessageDialog(frame,"Invalid email");
+				}
+				
+				/* // Date of Birth - Old Version
+				String dob= textField_2.getText(); // make sure valid date of birth
+				boolean check_dob=false;
+				try {
+					System.out.println(dob);
+					String[] temp=dob.split("/");					
+					System.out.println(temp[0]+temp[1]+temp[2]);
+					dob=LocalDate.of(Integer.parseInt(temp[2]), Integer.parseInt(temp[1]), Integer.parseInt(temp[0])).toString();
+					System.out.println("=="+dob);
+					if(isValidDOB(LocalDate.of(Integer.parseInt(temp[2]), Integer.parseInt(temp[1]), Integer.parseInt(temp[0])))) {
+						System.out.println("ch");
+						check_dob=true;
+					}
+					else {
+						JOptionPane.showMessageDialog(frame,"Invalid date of birth");
+					}
+				}
+		        catch (Exception e2) {
+		        	JOptionPane.showMessageDialog(frame,"Invalid date of birth");
+		        }	
+		        */	
+				
+				
+				if(email!=null && check_dob==true) {
+					//System.out.println(name+surname+email+password+sel_dob+gender+city+region);
+					java.sql.Statement st;
+					
+					try {// insert patient to database						
+						Connection myConn= DriverManager.getConnection("jdbc:sqlite:SupDocDB.db");
+						st = myConn.createStatement();
+						String q="INSERT INTO Patient VALUES('"+email+"','"+password+"','"+name+"','"+surname+"','"+sel_dob+"','"+gender+"','"+city+"','"+region+"',NULL,'true');";			
+						st.execute(q);						
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}				
+					
+					//Going to patient menu
+					login.email=email;
+					login.name=name;
+					patient_menu window = new patient_menu();
+					window.frame_patient_menu.setVisible(true);
+					frame.dispose();
+					
+										
+				}
+					
+					
+			}
+		});
 		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 		btnNewButton_1.setForeground(SystemColor.text);
 		btnNewButton_1.setBackground(SystemColor.textInactiveText);
-		btnNewButton_1.setBounds(77, 405, 104, 27);
+		btnNewButton_1.setBounds(91, 415, 104, 27);
 		frame.getContentPane().add(btnNewButton_1);
+		
+		
+		
+		
+		
+		
+		
 	}
+	
+	public static boolean isValid(String input_email){ // checking if email is valid
+        String emailRegex = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";                              
+        Pattern patt = Pattern.compile(emailRegex, Pattern.CASE_INSENSITIVE);
+        if (input_email == null)
+            return false;
+        return patt.matcher(input_email).matches(); //returns true if correct
+    }
+	
+	public static boolean isValidDOB(LocalDate input_dob){ // checking if date of birth is valid
+        LocalDate today=LocalDate.now();  
+        if(today.compareTo(input_dob)>=0)
+        	return true;
+        else
+        	return false;
+    }
 }
