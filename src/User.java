73546e3b1df09c1b3435773c744cea70ad8643c3
ariadni-machia <81858190+ariadni-machia@users.noreply.github.com>
2021-06-11 12:54:57 +0300

    User: Gender in greek +adding region attribute

--- src/User.java
+++ src/User.java
@@ -1,11 +1,12 @@
 import java.time.LocalDate; // import the LocalDate class
 import java.time.LocalTime;
+import java.awt.EventQueue;
 import java.io.File;  // Import the File class
 
 enum Gender {
-	  Female,
-	  Male,
-	  Other
+	  Γυναίκα,
+	  Άντρας,
+	  Άλλο
 	}
 
 public class User {
@@ -16,6 +17,7 @@ public class User {
 	protected LocalDate dateOfBirth;
 	protected Gender gender;
 	protected String city;
+	protected String region;
 	protected File profPhoto;
 	
 	public String getUserType(Object obj) { // Return user's type as a String (Patient or Doctor)
@@ -26,7 +28,7 @@ public class User {
 		
 	};
 	
-	public User(String email, String password, String name, String surname, String dateOfBirth, Gender gender, String city, File profPhot) {
+	public User(String email, String password, String name, String surname, String dateOfBirth, Gender gender, String city,String region, File profPhot) {
 		this.email=email;
 		this.password=password;
 		this.name=name;
@@ -34,12 +36,14 @@ public class User {
 		this.dateOfBirth=LocalDate.parse(dateOfBirth);
 		this.gender=gender;
 		this.city=city;
+		this.city=region;
 		this.profPhoto=profPhoto;
 	}
 	/*
 	public void printUser() {
-		System.out.println("email: "+this.email+"\n password: "+this.password+"\n name: "+this.name+"\n surname: "+this.surname+"\n date of Birth: "+this.dateOfBirth+"\n gender: "+this.gender+"\n city: "+this.city);
-		System.out.println("\n File: "+this.profPhoto);
+		System.out.println("email: "+this.email+"\n password: "+this.password+"\n name: "+this.name+"\n surname: "+this.surname+"\n date of Birth: "+this.dateOfBirth+"\n gender: "+this.gender+"\n city: "+this.city+"region: "+this.region);
+		//System.out.println("\n File: "+this.profPhoto);
 	}
 	*/
+	
 }
